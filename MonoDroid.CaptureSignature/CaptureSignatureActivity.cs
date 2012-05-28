6987868b8ac8886e56aef22c9c33e848df4ea992
Tomasz Cielecki <tomasz@ostebaronen.dk>
2012-05-28 23:30:30 +0200

    Added working sample.

---
+++ MonoDroid.CaptureSignature/CaptureSignatureActivity.cs
@@ -0,0 +1,254 @@
+﻿/*
+ * Origins from FingerPaint.java from the Android SDK API Samples and has been modified
+ * to be able to save canvas to SDcard and set stroke width of paint.
+ * 
+ * Copyright (C) 2007 The Android Open Source Project
+ * Copyright (C) 2012 Tomasz Cielecki (tomasz@ostebaronen.dk)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.IO;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Views;
+using Android.OS;
+using Android.Widget;
+
+namespace MonoDroid.CaptureSignature
+{
+    [Activity(Label = "Capture Signature", MainLauncher = true, Icon = "@drawable/icon")]
+    public class CaptureSignatureActivity : Activity
+    {
+        private CaptureSignatureView _capture;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            _capture = new CaptureSignatureView(this);
+            SetContentView(_capture);
+
+            _paint = new Paint
+                     {
+                         AntiAlias = true,
+                         Dither = true,
+                         Color = Color.Argb(255, 0, 0, 0)
+                     };
+            _paint.SetStyle(Paint.Style.Stroke);
+            _paint.StrokeJoin = Paint.Join.Round;
+            _paint.StrokeCap = Paint.Cap.Round;
+            _paint.StrokeWidth = 12;
+        }
+
+        public void ColorChanged(Color color)
+        {
+            _paint.Color = color;
+        }
+
+        private static Paint _paint;
+
+        public class CaptureSignatureView : View
+        {
+            private Bitmap _bitmap;
+            private Canvas _canvas;
+            private readonly Android.Graphics.Path _path;
+            private readonly Paint _bitmapPaint;
+
+            public CaptureSignatureView(Context c) : base(c)
+            {
+                _path = new Android.Graphics.Path();
+                _bitmapPaint = new Paint(PaintFlags.Dither);
+            }
+
+            protected override void OnSizeChanged(int w, int h, int oldw, int oldh)
+            {
+                base.OnSizeChanged(w, h, oldw, oldh);
+                _bitmap = Bitmap.CreateBitmap(w, h, Bitmap.Config.Argb8888);
+                _canvas = new Canvas(_bitmap);
+            }
+
+            protected override void OnDraw(Canvas canvas)
+            {
+                canvas.DrawColor(Color.White);
+
+                canvas.DrawBitmap(_bitmap, 0, 0, _bitmapPaint);
+
+                canvas.DrawPath(_path, _paint);
+            }
+
+            private float _mX, _mY;
+            private const float TouchTolerance = 4;
+
+            private void TouchStart(float x, float y)
+            {
+                _path.Reset();
+                _path.MoveTo(x, y);
+                _mX = x;
+                _mY = y;
+            }
+
+            private void TouchMove(float x, float y)
+            {
+                float dx = Math.Abs(x - _mX);
+                float dy = Math.Abs(y - _mY);
+
+                if (dx >= TouchTolerance || dy >= TouchTolerance)
+                {
+                    _path.QuadTo(_mX, _mY, (x + _mX) / 2, (y + _mY) / 2);
+                    _mX = x;
+                    _mY = y;
+                }
+            }
+
+            private void TouchUp()
+            {
+                _path.LineTo(_mX, _mY);
+                _canvas.DrawPath(_path, _paint);
+
+                _path.Reset();
+            }
+
+            public override bool OnTouchEvent(MotionEvent e)
+            {
+                var x = e.GetX();
+                var y = e.GetY();
+
+                switch (e.Action)
+                {
+                    case MotionEventActions.Down:
+                        TouchStart(x, y);
+                        Invalidate();
+                        break;
+                    case MotionEventActions.Move:
+                        TouchMove(x, y);
+                        Invalidate();
+                        break;
+                    case MotionEventActions.Up:
+                        TouchUp();
+                        Invalidate();
+                        break;
+                }
+                return true;
+            }
+
+            public void ClearCanvas()
+            {
+                _canvas.DrawColor(Color.White);
+                Invalidate();
+            }
+
+            public Bitmap CanvasBitmap()
+            {
+                return _bitmap;
+            }
+        }
+
+        private const int ColorMenuID = Menu.First;
+        private const int SelectStrokeWidthMenuID = Menu.First + 1;
+        private const int ClearCanvasMenuID = Menu.First + 2;
+        private const int SaveToSDCardID = Menu.First + 3;
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            menu.Add(0, ColorMenuID, 0, "Select Color").SetShortcut('3', 'c');
+            menu.Add(0, SelectStrokeWidthMenuID, 0, "Select Stroke Width").SetShortcut('4', 'w');
+            menu.Add(0, ClearCanvasMenuID, 0, "Clear Canvas").SetShortcut('5', 'e');
+            menu.Add(0, SaveToSDCardID, 0, "Save to External Storage").SetShortcut('6', 's');
+
+            return true;
+        }
+
+        public override bool OnPrepareOptionsMenu(IMenu menu)
+        {
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            _paint.SetXfermode(null);
+            _paint.Alpha = 255;
+
+            switch (item.ItemId)
+            {
+                case ColorMenuID:
+                    var cPicker = new ColorPickerDialog(this, _paint.Color);
+                    cPicker.ColorChanged += delegate(object sender, ColorChangedEventArgs args)
+                                            {
+                                                _paint.Color = args.Color;
+                                            };
+                    cPicker.Show();
+                    return true;
+                case ClearCanvasMenuID:
+                    _capture.ClearCanvas();
+                    return true;
+                case SaveToSDCardID:
+                    SaveToSd();
+                    return true;
+                case SelectStrokeWidthMenuID:
+                    StrokeDialog();
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void StrokeDialog()
+        {
+            var dialog = new Dialog(this);
+            dialog.SetTitle("Stroke width");
+            dialog.SetContentView(Resource.Layout.Main);
+            ((Spinner)dialog.FindViewById(Resource.Id.StrokeWidth)).ItemSelected += delegate(object sender, AdapterView.ItemSelectedEventArgs args)
+                                    {
+                                        var spinner = (Spinner)sender;
+                                        var strokeWidth = spinner.GetItemAtPosition(args.Position);
+                                        _paint.StrokeWidth = int.Parse(strokeWidth.ToString());
+                                    };
+            dialog.FindViewById(Resource.Id.StrokeOkButton).Click += (sender, args) => dialog.Dismiss();
+            dialog.Show();
+        }
+
+        /// <summary>
+        /// Saves the signature to the SDCard
+        /// </summary>
+        private void SaveToSd()
+        {
+            var folder = Android.OS.Environment.ExternalStorageDirectory + Java.IO.File.Separator + "MySignatures";
+            var extFileName = folder +
+                              Java.IO.File.Separator +
+                              Guid.NewGuid() + ".png";
+
+            try
+            {
+                if (!Directory.Exists(folder))
+                    Directory.CreateDirectory(folder);
+                using (var fs = new FileStream(extFileName, FileMode.OpenOrCreate))
+                {
+                    _capture.CanvasBitmap().Compress(Bitmap.CompressFormat.Png, 100, fs);
+                }    
+            }
+            catch
+            {
+                RunOnUiThread(() =>
+                              {
+                                  var builder = new AlertDialog.Builder(this);
+                                  builder.SetMessage("Saving signature went wrong");
+                                  builder.SetTitle("Unable to save signature");
+                                  builder.Show();
+                              });
+            }
+        }
+    }
+}
+
---
+++ MonoDroid.CaptureSignature/ColorPickerDialog.cs
@@ -0,0 +1,218 @@
+/*
+ * Derivative work of ColorPickerDialog.java from Android SDK API samples,
+ * ported to Mono for Android and added .NET style event handling.
+ * 
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Views;
+
+namespace MonoDroid.CaptureSignature
+{
+    public class ColorChangedEventArgs : EventArgs
+    {
+        public Color Color { get; set; }
+    }
+
+    public delegate void ColorChangedEventHandler(object sender, ColorChangedEventArgs args);
+
+    public class ColorPickerDialog : Dialog
+    {
+        public event ColorChangedEventHandler ColorChanged;
+        private static Color _initialColor;
+
+        public ColorPickerDialog(Context context, Color initialColor)
+            : base(context)
+        {
+            _initialColor = initialColor;
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var cPickerView = new ColorPickerView(Context, _initialColor);
+            cPickerView.ColorChanged += delegate(object sender, ColorChangedEventArgs args)
+                                        {
+                                            if (ColorChanged != null)
+                                                ColorChanged(this, args);
+
+                                            Dismiss();
+                                        };
+            SetContentView(cPickerView);
+            SetTitle("Pick a Color");
+        }
+
+        private class ColorPickerView : View
+        {
+
+            public event ColorChangedEventHandler ColorChanged;
+            private readonly Paint _paint;
+            private readonly Paint _centerPaint;
+            private readonly int[] _colors;
+
+            public ColorPickerView(Context c, Color color)
+                : base(c)
+            {
+                _colors = new[] {
+                    new Color(255,0,0,255).ToArgb(), new Color(255,0,255,255).ToArgb(), new Color(0,0,255,255).ToArgb(),
+                    new Color(0,255,255,255).ToArgb(), new Color(0,255,0,255).ToArgb(), new Color(255,255,0,255).ToArgb(),
+                    new Color(255,0,0,255).ToArgb()
+                };
+                Shader s = new SweepGradient(0, 0, _colors, null);
+
+                _paint = new Paint(PaintFlags.AntiAlias);
+                _paint.SetShader(s);
+                _paint.SetStyle(Paint.Style.Stroke);
+                _paint.StrokeWidth = 32;
+
+                _centerPaint = new Paint(PaintFlags.AntiAlias)
+                               {
+                                   Color = color,
+                                   StrokeWidth = 5
+                               };
+            }
+
+            private bool _trackingCenter;
+            private bool _highlightCenter;
+
+            protected override void OnDraw(Canvas canvas)
+            {
+                var r = CenterX - _paint.StrokeWidth * 0.5f;
+
+                canvas.Translate(CenterX, CenterX);
+
+                canvas.DrawOval(new RectF(-r, -r, r, r), _paint);
+                canvas.DrawCircle(0, 0, CenterRadius, _centerPaint);
+
+                if (_trackingCenter)
+                {
+                    var c = _centerPaint.Color;
+                    _centerPaint.SetStyle(Paint.Style.Stroke);
+
+                    _centerPaint.Alpha = _highlightCenter ? 255 : 128;
+
+                    canvas.DrawCircle(0, 0,
+                                      CenterRadius + _centerPaint.StrokeWidth,
+                                      _centerPaint);
+
+                    _centerPaint.SetStyle(Paint.Style.Fill);
+                    _centerPaint.Color = c;
+                }
+            }
+
+            protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
+            {
+                SetMeasuredDimension(CenterX * 2, CenterY * 2);
+            }
+
+            private const int CenterX = 100;
+            private const int CenterY = 100;
+            private const int CenterRadius = 32;
+
+            private static int Ave(int s, int d, float p)
+            {
+                return (int)(s + Math.Round(p * (d - s)));
+            }
+
+            private static int InterpColor(IList<int> colors, float unit)
+            {
+                if (unit <= 0)
+                {
+                    return colors[0];
+                }
+                if (unit >= 1)
+                {
+                    return colors[colors.Count - 1];
+                }
+
+                float p = unit * (colors.Count - 1);
+                var i = (int)p;
+                p -= i;
+
+                // now p is just the fractional part [0...1) and i is the index
+                int c0 = colors[i];
+                int c1 = colors[i + 1];
+                int a = Ave(Color.GetAlphaComponent(c0), Color.GetAlphaComponent(c1), p);
+                int r = Ave(Color.GetRedComponent(c0), Color.GetRedComponent(c1), p);
+                int g = Ave(Color.GetGreenComponent(c0), Color.GetGreenComponent(c1), p);
+                int b = Ave(Color.GetBlueComponent(c0), Color.GetBlueComponent(c1), p);
+
+                return Color.Argb(a, r, g, b);
+            }
+
+            private const float PI = 3.1415926f;
+
+            public override bool OnTouchEvent(MotionEvent e)
+            {
+                float x = e.GetX() - CenterX;
+                float y = e.GetY() - CenterY;
+                bool inCenter = Math.Sqrt(x * x + y * y) <= CenterRadius;
+
+                switch (e.Action)
+                {
+                    case MotionEventActions.Down:
+                        _trackingCenter = inCenter;
+                        if (inCenter)
+                        {
+                            _highlightCenter = true;
+                            Invalidate();
+                        }
+                        break;
+                    case MotionEventActions.Move:
+                        if (_trackingCenter)
+                        {
+                            if (_highlightCenter != inCenter)
+                            {
+                                _highlightCenter = inCenter;
+                                Invalidate();
+                            }
+                        }
+                        else
+                        {
+                            var angle = (float)Math.Atan2(y, x);
+                            // need to turn angle [-PI ... PI] into unit [0....1]
+                            var unit = angle / (2 * PI);
+                            if (unit < 0)
+                            {
+                                unit += 1;
+                            }
+                            _centerPaint.Color = new Color(InterpColor(_colors, unit));
+                            Invalidate();
+                        }
+                        break;
+                    case MotionEventActions.Up:
+                        if (_trackingCenter)
+                        {
+                            if (inCenter)
+                            {
+                                if (null != ColorChanged)
+                                    ColorChanged(this, new ColorChangedEventArgs { Color = _centerPaint.Color });
+                            }
+                            _trackingCenter = false;    // so we draw w/o halo
+                            Invalidate();
+                        }
+                        break;
+                }
+                return true;
+            }
+        }
+    }
+}
